a3128295fde080e6c9a5ea794bd33be2ab7fb3c5
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-13 16:06:50 +0200

    Update firebase.js

--- wwwsys/app/js/S2.Framework/Firebase/firebase.js
+++ wwwsys/app/js/S2.Framework/Firebase/firebase.js
@@ -1,19 +1,20 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+//<script type="module">
+    
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA7gp1DEmyAkF-yLH8m_r97MLE1CIuISxE",
-    authDomain: "s2design-team-website.firebaseapp.com",
-    projectId: "s2design-team-website",
-    storageBucket: "s2design-team-website.appspot.com",
-    messagingSenderId: "68864723125",
-    appId: "1:68864723125:web:d782c2accb4159545d16fc"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7gp1DEmyAkF-yLH8m_r97MLE1CIuISxE",
+  authDomain: "s2design-team-website.firebaseapp.com",
+  projectId: "s2design-team-website",
+  storageBucket: "s2design-team-website.appspot.com",
+  messagingSenderId: "68864723125",
+  appId: "1:68864723125:web:d782c2accb4159545d16fc"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//</script>
