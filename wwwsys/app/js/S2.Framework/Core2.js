87eb98905cf695e84b6febc29d82cbd2404abd22
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-20 19:44:28 +0200

    Update Core2.js

--- wwwsys/app/js/S2.Framework/Core2.js
+++ wwwsys/app/js/S2.Framework/Core2.js
@@ -1,1 +1,23 @@
-import { TypeOfCheck } from './Core/TypeCheck.js';
+// Import TypeCheck class from TypeOfCheck.js
+import { TypeOfCheck } from './Core/TypeOfCheck.js';
+
+// Import HttpClient  class from HttpClientk.js
+import { HttpClient } from './Core/HttpClient.js';
+
+// Import BaseComponent  class from BaseComponent.js
+import { BaseComponent } from './Core/BaseComponent .js';
+
+// Function to be executed after the page has fully loaded
+function onPageLoad() {
+    // Load program.js after page load
+    const script = document.createElement('script');
+    script.src = '/program.js';
+    document.body.appendChild(script);
+}
+
+// Check if the document has fully loaded
+if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
+    onPageLoad();
+} else {
+    document.addEventListener('DOMContentLoaded', onPageLoad);
+}
