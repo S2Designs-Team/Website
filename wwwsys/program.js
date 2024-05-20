7e4d52b77cc67d33e6c1654c417a6f10411c90fe
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-20 21:16:09 +0200

    Create program.js

---
+++ wwwsys/program.js
@@ -0,0 +1,9 @@
+// Import of needed graphic components
+import './app/js/S2.Framework/Components/Component1.js';
+import './app/js/S2.Framework/Components/Component2.js';
+
+// Istanze dei componenti grafici
+const component1 = new Component1();
+const component2 = new Component2();
+component1.render();
+component2.render();
