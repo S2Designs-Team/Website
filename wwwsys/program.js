5f43eef2ac609b68025ce41cc316bcf1e17defa7
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-20 21:29:43 +0200

    Update program.js

--- wwwsys/program.js
+++ wwwsys/program.js
@@ -1,9 +1,6 @@
 // Import of needed graphic components
 import './app/js/S2.Framework/Components/Component1.js';
-import './app/js/S2.Framework/Components/Component2.js';
 
 // Istanze dei componenti grafici
 const component1 = new Component1();
-const component2 = new Component2();
-component1.render();
-component2.render();
+component1.render("conent");
