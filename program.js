895e8a852315a01106558ac870b8b53e3f7078e3
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-23 01:37:27 +0200

    Update program.js

--- program.js
+++ program.js
@@ -52,12 +52,13 @@ console.log("# Created by S2DesignsTeam © 2035 (Salvatore Nillo AKA ㊙️anoni
             "# ⏱️ ASYNCHRONISM                                                         " + "</BR>" +
             "# ⌚ TIMER LOOP                                                           " + "</BR>" +
             "###########################################################################");
-
+/*
 if (Application.isHTA()) {
     console.info(System.getJavascriptVersion() + " | " + System.getJScriptVersion() + "." );
 } else {
     console.info(System.getJavascriptVersion() + "." );
 }
+*/
 
 console.info("Application Start.");
 console.info(AppContext.isLocallyHosted(window.location.href) ? "Web app running locally from " + AppContext.getStartPath() + "</BR>" : 
