628f7bb56d4115539622363a78461741060daf9a
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-23 01:31:09 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,25 +1,72 @@
 // Import of needed graphic components
 import { Component1 } from './wwwsys/app/js/S2.Framework/Components/Component1.js';
 
+// Sets a proprerty using an object 
+const config = { apiUrl: 'https://api.example.com', timeout: 5000 };
+
 AppContext.setProperty("landingPageUrl"          , "wwwsys/pages/index.html");
 AppContext.setProperty("name"                    , "My application");
 AppContext.setProperty("contentContainerDomName" , "content");
 AppContext.setProperty("isLocalContent"          , "true");
-
-// Sets a proprerty using an object 
-const config = { apiUrl: 'https://api.example.com', timeout: 5000 };
 AppContext.setProperty(config);
 
-console.info("Application Start.");
-/*
-*AppHelper.wrapAllRoutes();
+/* 🛠️ DEBUG PURPOSE ONLY
+console.log("applicationName  = " + oHta.applicationName + "<br>" + 
+              "border           = " + oHta.border          + "<br>" +
+              "borderStyle      = " + oHta.borderStyle     + "<br>" + 
+              "caption          = " + oHta.caption         + "<br>" +
+              "commandLine      = " + oHta.commandLine     + "<br>" +
+              "icon             = " + oHta.icon            + "<br>" +
+              "maximizeButton   = " + oHta.maximizeButton  + "<br>" +
+              "minimizeButton   = " + oHta.minimizeButton  + "<br>" + 
+              "showInTaskBar    = " + oHta.showInTaskbar   + "<br>" +
+              "singleInstance   = " + oHta.singleInstance  + "<br>" +  
+              "sysMenu          = " + oHta.sysMenu         + "<br>" + 
+              "version          = " + oHta.version         + "<br>" + 
+              "windowState      = " + oHta.windowState     + "<br>" );
 */
-console.info("Redirecting to the landing page....");
+console.log("# Created by S2DesignsTeam © 2035 (Salvatore Nillo AKA ㊙️anonimo㊙️).      " + "</BR>" +
+            "#                                                                          " + "</BR>" +
+            "# Licensed under the Apache License, Version 2.0 (the 'License');          " + "</BR>" +
+            "# you may not use this file except in compliance with the License.         " + "</BR>" +
+            "# You may obtain a copy of the License at                                  " + "</BR>" +
+            "#                                                                          " + "</BR>" +
+            "#      http://www.apache.org/licenses/LICENSE-2.0                          " + "</BR>" +
+            "#                                                                          " + "</BR>" +
+            "# Unless required by applicable law or agreed to in writing, software      " + "</BR>" +
+            "# distributed under the License is distributed on an 'AS IS' BASIS,        " + "</BR>" +
+            "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. " + "</BR>" +
+            "# See the License for the specific language governing permissions and      " + "</BR>" +
+            "# limitations under the License.                                           " + "</BR>" +
+            "#                                                                          " + "</BR>" +
+            "# Symbol legend:                                                           " + "</BR>" +
+            "# 🛠️ DEBUG PURPOSE ONLY                                                   " + "</BR>" +
+            "# ⚙️ SETUP                                                                " + "</BR>" +
+            "# 🧪 EXPERIMENTAL                                                         " + "</BR>" +
+            "# 💻 SYSTEM FUNCTION                                                      " + "</BR>" +
+            "# 🔍 TO INVESTIGATE                                                       " + "</BR>" +
+            "# 💡 BRILLIANT IDEA                                                       " + "</BR>" +
+            "# 📜 STEP EXPLANATION                                                     " + "</BR>" +
+            "# 📎 DOCUMENTATION COMMENT                                                " + "</BR>" +
+            "# ⏰ USED AS DELEGATE TRIGGERED ELSEWHERE                                 " + "</BR>" +
+            "# ⏱️ ASYNCHRONISM                                                         " + "</BR>" +
+            "# ⌚ TIMER LOOP                                                           " + "</BR>" +
+            "###########################################################################");
 
-/*
-* AppHelper.loadUrl(AppContext.getProperty["landingPageUrl"]);
-*/
+if (Application.isHTA()) {
+    console.info(System.getJavascriptVersion() + " | " + System.getJScriptVersion() + "." );
+} else {
+    console.info(System.getJavascriptVersion() + "." );
+}
+
+console.info("Application Start.");
+console.info(AppContext.isLocallyHosted(window.location.href) ? "Web app running locally from " + AppContext.getStartPath() + "</BR>" : 
+                                                                "Web app running remotely at "  + AppContext.getBaseUrl()   + "</BR>");
+
+// Export the singleton instance
+const appHttpClient = new HttpClient();
 
 // Istanze dei componenti grafici
 const component1 = new Component1();
 component1.render("content");
+console.info("Redirecting to the landing page....");
