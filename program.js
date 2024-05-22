313ca634a9f61172e469de6f6b7f5ee166e3aee0
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-22 21:08:44 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,7 +1,10 @@
 // Import of needed graphic components
 import { Component1 } from './wwwsys/app/js/S2.Framework/Components/Component1.js';
 
-AppContext.addProperty("landingPageUrl");
+AppContext.addProperty({ name: "landingPageUrl"          , value: "wwwsys/pages/index.html" });
+AppContext.addProperty({ name: "name"                    , value: "My application"} );
+AppContext.addProperty({ name: "contentContainerDomName" , value: ".content"} );
+AppContext.addProperty({ name: "isLocalContent"          , value: "true"} );
 
 // Istanze dei componenti grafici
 const component1 = new Component1();
