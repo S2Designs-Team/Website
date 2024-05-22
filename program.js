3867c0a2dc36dc5f450b486468b89320100bc393
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-22 15:54:27 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
 // Import of needed graphic components
 import { Component1 } from './wwwsys/app/js/S2.Framework/Components/Component1.js';
 
+AppContext.addProperty("landingPageUrl");
+
 // Istanze dei componenti grafici
 const component1 = new Component1();
 component1.render("content");
