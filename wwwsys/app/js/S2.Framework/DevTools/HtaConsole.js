582ac3e849757b47c83570b808185253cc9f07af
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-24 00:02:55 +0200

    Update HtaConsole.js

--- wwwsys/app/js/S2.Framework/DevTools/HtaConsole.js
+++ wwwsys/app/js/S2.Framework/DevTools/HtaConsole.js
@@ -7,80 +7,82 @@ class HtaConsole extends BaseComponent {
         }
 
         this.htmlSegment = `
-            <DIV ID='panel-box'>
-                <DIV ID='panel-dragbar'></DIV>
-                <DIV ID='panel-header'>
-                    <SPAN ID='panel-menu-clear' STYLE='cursor: pointer'>&#10680;</SPAN>
-                    <SPAN ID='panel-menu-transparent' STYLE='cursor: pointer'>&#9680;</SPAN>
-                    <SPAN ID='panel-menu-reload' STYLE='cursor: pointer'>&#10227;</SPAN>
-                    <SPAN STYLE='padding-left: 5px; font-weight: 700; color: #555;'>Console</SPAN>
-                    <SPAN ID='panel-menu-close' STYLE='float: right; margin-right: 8px; cursor: pointer; color: grey;'>&#10006;</SPAN>
-                    <SPAN ID='panel-menu-minimize' STYLE='float: right; margin-top:1px;margin-right: 10px; cursor: pointer; color: grey;'>&#9660;</SPAN>
+            <DIV ID='console-panel'>
+                <DIV ID='console-dragBorder'></DIV>
+                <DIV ID='console-titleBar'>
+                    <SPAN ID='console-titleBar-btnClear'    STYLE='cursor: pointer'>&#10680;</SPAN>
+                    <SPAN ID='console-titleBar-btnOpacity'  STYLE='cursor: pointer'>&#9680;</SPAN>
+                    <SPAN ID='console-titleBar-btnReload'   STYLE='cursor: pointer'>&#10227;</SPAN>
+                    <SPAN ID='console-titleBar-caption'     STYLE='padding-left: 5px; font-weight: 700; color: #555;'>Console</SPAN>
+                    <SPAN ID='console-titleBar-btnClose'    STYLE='float: right; margin-right: 8px; cursor: pointer; color: grey;'>&#10006;</SPAN>
+                    <SPAN ID='console-titleBar-btnMinimize' STYLE='float: right; margin-top:1px;margin-right: 10px; cursor: pointer; color: grey;'>&#9660;</SPAN>
                 </DIV>
-                <DIV STYLE='background: rgba(19,19,19,1);'>
+                <DIV ID='console-history-panel' STYLE='background: rgba(19,19,19,1);'>
                     <DIV ID='panel-console'></DIV>
-                    <SPAN STYLE='border-top:1px solid grey; min-height: 1.5em; padding-left:4px; font-weight:700; font-size:16px; color:#62adea'>&#62;</I><INPUT ID='commandline'></SPAN>
+                    <SPAN STYLE='border-top:1px solid grey; min-height: 1.5em; padding-left:4px; font-weight:700; font-size:16px; color:#62adea'>&#62;
+                        </I><INPUT ID='commandline'>
+                    </SPAN>
                 </DIV>
                 <DIV ID='panel-navigation'></DIV>
             </DIV>
         `;
 
         this.cssStyles = `
-            #panel-box {
-                width: 100%;
-                color: #000000;
-                font-family: Consolas, Courier;
-                font-size: 1em;
-                padding: 3px 0px 3px 0px;
-                position: fixed;
-                bottom: -4px;
-                left: 0px;
-                opacity: 1.0;
-                display: block;
-                z-index: 10000;
+            #console-panel {
+                width:         100%;
+                color:         #000000;
+                font-family:   Consolas, Courier;
+                font-size:     1em;
+                padding:       3px 0px 3px 0px;
+                position:      fixed;
+                bottom:        -4px;
+                left:          0px;
+                opacity:       1.0;
+                display:       block;
+                z-index:       10000;
             }
-            #panel-dragbar {
-                border-top: 3px solid #ededed;
+            #console-dragBorder {
+                border-top:    3px solid #ededed;
                 border-bottom: 1px solid #ccc;
-                width: 100%;
-                cursor: row-resize;
+                width:         100%;
+                cursor:        row-resize;
             }
-            #panel-header {
-                height: 24px;
-                background: #ededed;
-                border-top: 1px solid #fff;
+            #console-titleBar{
+                height:        24px;
+                background:    #ededed;
+                border-top:    1px solid #fff;
                 border-bottom: 1px solid #cacaca;
-                text-align: left;
-                font-family: Arial;
+                text-align:    left;
+                font-family:   Arial;
             }
             #panel-console {
-                display: block;
-                overflow: auto;
-                height: 150px;
-                color: #62ADEA;
-                text-align: left;
-                padding: 0 5px 0 5px;
-                background: rgba(19,19,19,1);
+                display:       block;
+                overflow:      auto;
+                height:        150px;
+                color:         #62ADEA;
+                text-align:    left;
+                padding:       0 5px 0 5px;
+                background:    rgba(19,19,19,1);
             }
             #panel-navigation {
-                height: 21px;
-                width: 100%;
-                padding-left: 4px;
-                line-height: 1;
-                font-size: 18px;
-                font-weight: 700;
-                background: #ededed;
-                border-top: 1px solid #cacaca;
-                font-family: Arial;
-                color: grey;
+                height:        21px;
+                width:         100%;
+                padding-left:  4px;
+                line-height:   1;
+                font-size:     18px;
+                font-weight:   700;
+                background:    #ededed;
+                border-top:    1px solid #cacaca;
+                font-family:   Arial;
+                color:         grey;
             }
             #commandline {
-                width: 97%;
-                border: none;
-                color: #62ADEA;
-                padding-left: 6px;
+                width:         97%;
+                border:        none;
+                color:         #62ADEA;
+                padding-left:  6px;
                 outline-width: 0px;
-                background: rgba(19,19,19,0);
+                background:    rgba(19,19,19,0);
             }
         `;
 
@@ -92,37 +94,7 @@ class HtaConsole extends BaseComponent {
     }
 
     initialize() {
-        this.createTooltip("panel-menu-reload", "Reload the page avoiding cached data.");
-
-        /*
-        var element = document.getElementById("panel-dragbar");
-        element.addEventListener("mousedown", this.dragHandler);
-        element.addEventListener("mouseup", () => {
-            element.removeEventListener("mousemove", this.dragHandler);
-        });
-        */
-        /*
-        * Manages if the keyboard F12 button has been pressed Showing/hiding the console
-        */
-        /*
-        document.addEventListener("keydown", (evt) => {
-            if (evt.keyCode === 123) this.toggle();
-        }, true);
-        */
-        /*
-        * Manages the click on the console area placing the focus to the 'command line' 
-        */
-        /*document.getElementById("panel-console").addEventListener("click", () => {
-            document.getElementById("commandline").focus();
-        });
-        */
-        /*
-        * 
-        */
-        /*
-        document.getElementById('commandline').addEventListener('keypress', this.commandLineKeyPressHandler);
-        document.getElementById('commandline').addEventListener('keydown', this.commandLineKeyDownHandler);
-        */
+        this.createTooltip("console-titleBar-btnReload", "Reload the page avoiding cached data.");
         console = window.console;
         const method = [
             "log", "info", "warn", "onerror", "debug", "trace", "dir", "group",
@@ -165,10 +137,10 @@ class HtaConsole extends BaseComponent {
         /* */
         document.addEventListener("keydown", this.toggle, true); //The handler is executed in the capturing phase.
         /* */
-        document.getElementById("panel-dragbar")
+        document.getElementById("console-dragBorder")
             .addEventListener("mousedown", this.dragHandler);
         /* */
-        document.getElementById("panel-dragbar")
+        document.getElementById("console-dragBorder")
             .addEventListener("mouseup",   this.releaseDragHandler);
         /* Manages the click on the console area placing the focus to the 'command line'  */
         document.getElementById("panel-console")
@@ -180,16 +152,16 @@ class HtaConsole extends BaseComponent {
         document.getElementById('commandline')
             .addEventListener('keydown', this.commandLineKeyDownHandler);
         /* */
-        document.getElementById('panel-menu-clear')
+        document.getElementById('console-titleBar-btnClear')
             .addEventListener('click', this.clear);
         /* */
-        document.getElementById('panel-menu-transparent')
+        document.getElementById('console-titleBar-btnOpacity')
             .addEventListener('click', this.transparent);
         /* */
-        document.getElementById('panel-menu-reload')
+        document.getElementById('console-titleBar-btnReload')
             .addEventListener('click', this.reload);
         /* */
-        document.getElementById('panel-menu-close')
+        document.getElementById('console-titleBar-btnClose')
             .addEventListener('click', this.toggle);
         /* */
         document.getElementById('panel-menu-minimize')
@@ -202,7 +174,7 @@ class HtaConsole extends BaseComponent {
     */ 
     toggle = (evt) => {
         if (evt.keyCode === 123) {
-            const el = document.getElementById("panel-box");
+            const el = document.getElementById("console-panel");
             el.style.display = (el.style.display !== "none") ? "none" : "block";
         }
     }
@@ -211,7 +183,7 @@ class HtaConsole extends BaseComponent {
         document.getElementById("panel-console").style.height = `${height - evt.clientY}px`;
     }
     releaseDragHandler = () => {
-        document.getElementById("panel-dragbar").removeEventListener("mousemove", this.dragHandler); 
+        document.getElementById("console-dragBorder").removeEventListener("mousemove", this.dragHandler); 
     }
     setFocus = () =>{
         document.getElementById("commandline").focus();
@@ -280,12 +252,12 @@ class HtaConsole extends BaseComponent {
     }
 
     minimize = () => {
-        const el = document.getElementById("panel-box");
+        const el = document.getElementById("console-panel");
         el.style.height = (el.style.height !== "20px") ? "20px" : "220px";
     }
 
     transparent = () => {
-        const el = document.getElementById("panel-box");
+        const el = document.getElementById("console-panel");
         el.style.opacity = (el.style.opacity !== "0.5") ? "0.5" : "1.0";
     }
     
