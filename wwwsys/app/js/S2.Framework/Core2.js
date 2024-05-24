984ad0011a4b36e0884f8bce30dab18f063a0ff3
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-24 12:16:51 +0200

    Update Core2.js

--- wwwsys/app/js/S2.Framework/Core2.js
+++ wwwsys/app/js/S2.Framework/Core2.js
@@ -4,17 +4,12 @@ import { HttpClient }    from './Core/HttpClient.js';     // Import HttpClient c
 import { AppContext }    from './Core/AppContext.js';     // Import AppContext instance from AppContext.js
 import { HtaConsole }    from './DevTools/HtaConsole.js'; // Import HtaConsole instance from HtaConsole.js
 
-/*
-* Exports BaseComponent, System, HttpClient, AppContext, HtaConsole to be accessible globally
-*/
-export { BaseComponent, System, HttpClient, AppContext, HtaConsole };
-
 // Function to be executed after the page has fully loaded
 function loadMainScript() {
     window.BaseComponent = BaseComponent;
     window.System        = System;
     window.HttpClient    = HttpClient;
-    window.AppContext    = AppContext;    
+    window.AppContext    = AppContext;
     window.HtaConsole    = HtaConsole;
     
     // Get the base URL of the current page
@@ -32,3 +27,8 @@ if (document.readyState === 'complete' || (document.readyState !== 'loading' && 
 } else {
     document.addEventListener('DOMContentLoaded', loadMainScript);
 }
+
+/*
+* Exports BaseComponent, System, HttpClient, AppContext, HtaConsole to be accessible globally
+*/
+export { BaseComponent, System, HttpClient, AppContext, HtaConsole };
