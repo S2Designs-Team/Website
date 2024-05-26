6576b1998cc2a0cbdd57fea6c9cd8cf3f0cc99a4
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-27 01:21:43 +0200

    Update Core2.js

--- wwwsys/app/js/S2.Framework/Core2.js
+++ wwwsys/app/js/S2.Framework/Core2.js
@@ -1,4 +1,4 @@
-import { jQuery }        from './Core/JQuery.Module.js';  // Import $ from JQuery.Module.js
+import { JQuery }        from './Core/JQuery.Module.js';  // Import $ from JQuery.Module.js
 import { StringHelper }  from './Core/StringHelper.js';   // Import StringHelper class from SpaHelper.js
 import { BaseComponent } from './Core/BaseComponent.js';  // Import BaseComponent class from BaseComponent.js
 import { System }        from './Core/System.js';         // Import System class from System.js
@@ -14,7 +14,7 @@ function startApplication() {
     loadMainScript();
 }
 function loadCoreServices() {
-    window.$             = jQuery;
+    window.$             = JQuery;
     window.String        = StringHelper;    
     window.BaseComponent = BaseComponent;
     window.System        = System;
@@ -43,4 +43,4 @@ if (document.readyState === 'complete' || (document.readyState !== 'loading' && 
 /*
 * Exports BaseComponent, System, HttpClient, AppContext, HtaConsole to be accessible globally
 */
-export { BaseComponent, System, HttpClient, AppContext, HtaConsole, SpaHelper, StringHelper as String, $ };
+export { BaseComponent, System, HttpClient, AppContext, HtaConsole, SpaHelper, StringHelper as String, jQuery as $ };
