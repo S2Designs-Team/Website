e674d609281d4bc3a4a79993b9747e07da2815fe
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-26 03:21:53 +0200

    Update Core2.js

--- wwwsys/app/js/S2.Framework/Core2.js
+++ wwwsys/app/js/S2.Framework/Core2.js
@@ -18,7 +18,7 @@ function loadCoreServices() {
     window.HttpClient    = HttpClient;
     window.AppContext    = AppContext;
     window.HtaConsole    = HtaConsole;
-    window.SpaHelper     = SpaHelper;
+    window.SpaHelper     = new _SpaHelper();
 }
 function loadMainScript() {
     // Get the base URL of the current page
