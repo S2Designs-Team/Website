8ca9cc00111d7211aec2ab14615de4c6dc115a6f
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-27 01:32:50 +0200

    Update JQuery.Module.js

--- wwwsys/app/js/S2.Framework/Core/JQuery.Module.js
+++ wwwsys/app/js/S2.Framework/Core/JQuery.Module.js
@@ -1,3 +1,3 @@
 // jquery.module.js
-import 'https://code.jquery.com/jquery-3.6.0.min.js'
-export  { jQuery.noConflict(true) as JQuery }
+import $ from 'https://code.jquery.com/jquery-3.6.0.min.js'
+export  { $ as JQuery }
