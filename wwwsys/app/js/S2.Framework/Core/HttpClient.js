823a8ab285e2895b34d3b610069c8d534c30300a
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-27 00:12:42 +0200

    Update HttpClient.js

--- wwwsys/app/js/S2.Framework/Core/HttpClient.js
+++ wwwsys/app/js/S2.Framework/Core/HttpClient.js
@@ -136,4 +136,30 @@ export class HttpClient {
         });
         return response.json();
     }
+    loadHtmlPage = async (url) =>{
+        if (url) {
+          fetch(url)
+              .then( (response) => {
+                  // When the page is loaded convert it to text
+                  return response.text()
+              })
+              .then( (html) => {
+                  // Initialize the DOM parser
+                  var parser = new DOMParser();
+          
+                  // Parse the text
+                  var doc = parser.parseFromString(html, "text/html");
+          
+                  // You can now even select part of that html as you would in the regular DOM 
+                  // Example:
+                  // var docArticle = doc.querySelector('article').innerHTML;
+          
+                  console.log(doc);
+                  return doc;
+              })
+              .catch(function(err) {  
+                  console.log('Failed to fetch page: ', err);  
+              });
+        }
+    }
 }
