2785453e0e72cb64e65805f863d405862f188149
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-28 17:03:53 +0200

    Update ChannelsEpg.js

--- wwwsys/app/js/S2.Framework/TvAndVideoPlayer/ChannelsEpg.js
+++ wwwsys/app/js/S2.Framework/TvAndVideoPlayer/ChannelsEpg.js
@@ -52,7 +52,7 @@ class ChannelsEpg {
 			};
 			_httpRequest.get().then(data => {
 				console.log('Risposta GET:', data);
-				this.#EpgData["Rai"] = data;
+				this.EpgData["Rai"] = data;
 			});
 			/*
 			fetch(this.#EPG_URL_RAI).
@@ -79,7 +79,7 @@ class ChannelsEpg {
 			};
 			_httpRequest.get().then(data => {
 				console.log('Risposta GET:', data);
-				this.#EpgData["Mediaset"] = data;
+				this.EpgData["Mediaset"] = data;
 			});
 			/*
 			fetch(this.#EPG_URL_MEDIASET).
@@ -145,77 +145,77 @@ class ChannelsEpg {
 	applyChannelsEPG = () => {
 		$("[id='Rai 1']").
 			find('#title').
-				html(this.#EpgData["Rai"].on_air[0].currentItem.name);
+				html(this.EpgData["Rai"].on_air[0].currentItem.name);
 		$("[id='Rai 2']").
 			find('#title').
-				html(this.#EpgData["Rai"].on_air[1].currentItem.name);
+				html(this.EpgData["Rai"].on_air[1].currentItem.name);
 		$("[id='Rai 3']").
 			find('#title').
-				html(this.#EpgData["Rai"].on_air[2].currentItem.name);
+				html(this.EpgData["Rai"].on_air[2].currentItem.name);
 		$("[id='Rai 4']").
 			find('#title').
-				html(this.#EpgData["Rai"].on_air[3].currentItem.name);
+				html(this.EpgData["Rai"].on_air[3].currentItem.name);
 		//========================================================================================================================
 		$("[id='Mediaset - Rete 4']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.R4.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.R4.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Canale 5']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.C5.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.C5.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Italia 1']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.I1.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.I1.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Italia 2']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.I2.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.I2.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Italia America']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.MW.currentListing.mediasetlisting$epgTitle);	
+				html(this.EpgData["Mediaset"].response.listings.MW.currentListing.mediasetlisting$epgTitle);	
 		$("[id='Mediaset - Focus']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.FU.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.FU.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Extra']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.KQ.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.KQ.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - La5']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.KA.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.KA.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - 20']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.LB.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.LB.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - 27']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.TS.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.TS.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Iris']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.KI.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.KI.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Top Crime']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.LT.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.LT.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Cine34']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.B6.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.B6.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Boing']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.KB.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.KB.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Cartoonito']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.LA.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.LA.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - TG.COM 24']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.KF.currentListing.mediasetlisting$epgTitle);
+				html(this.EpgData["Mediaset"].response.listings.KF.currentListing.mediasetlisting$epgTitle);
 		$("[id='Mediaset - Radio 101']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.ER.currentListing.mediasetlisting$epgTitle + "<BR />" + 
-				     this.#EpgData["Mediaset"].response.listings.ER.currentListing.mediasetlisting$shortDescription);
+				html(this.EpgData["Mediaset"].response.listings.ER.currentListing.mediasetlisting$epgTitle + "<BR />" + 
+				     this.EpgData["Mediaset"].response.listings.ER.currentListing.mediasetlisting$shortDescription);
 		$("[id='Mediaset - Radio 105']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.EC.currentListing.mediasetlisting$epgTitle + "<BR />" + 
-				     this.#EpgData["Mediaset"].response.listings.EC.currentListing.mediasetlisting$shortDescription);
+				html(this.EpgData["Mediaset"].response.listings.EC.currentListing.mediasetlisting$epgTitle + "<BR />" + 
+				     this.EpgData["Mediaset"].response.listings.EC.currentListing.mediasetlisting$shortDescription);
 		$("[id='Mediaset - Virgin Radio']").
 			find('#title').
-				html(this.#EpgData["Mediaset"].response.listings.EW.currentListing.mediasetlisting$epgTitle + "<BR />" + 
-				     this.#EpgData["Mediaset"].response.listings.EW.currentListing.mediasetlisting$shortDescription);
+				html(this.EpgData["Mediaset"].response.listings.EW.currentListing.mediasetlisting$epgTitle + "<BR />" + 
+				     this.EpgData["Mediaset"].response.listings.EW.currentListing.mediasetlisting$shortDescription);
 		//$("#radioMonteCarlo-title").html(epgDataMediaset.response.listings.BB.currentListing.mediasetlisting$epgTitle + "<BR />" + epgDataMediaset.response.listings.BB.currentListing.mediasetlisting$shortDescription);
 		//========================================================================================================================
 		/*
