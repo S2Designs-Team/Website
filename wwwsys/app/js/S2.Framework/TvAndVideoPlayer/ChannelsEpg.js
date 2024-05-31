2c238b6afb8b0c26f515df91add05b9fc71fd871
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-05-31 15:28:05 +0200

    .

--- wwwsys/app/js/S2.Framework/TvAndVideoPlayer/ChannelsEpg.js
+++ wwwsys/app/js/S2.Framework/TvAndVideoPlayer/ChannelsEpg.js
@@ -42,7 +42,7 @@ class ChannelsEpg {
                                            "locale=it&" +
                                            "market_code=it&" +
                                            "per_page=120";
-            this.update();
+            await this.update();
             this.applyChannelsEPG();
             this.startPolling();
         } catch (error){
