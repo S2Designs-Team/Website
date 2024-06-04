972d2ffc4e226f090582710742fd4308cf5c639a
Phobetor1999 <89788249+Phobetor1999@users.noreply.github.com>
2024-06-04 16:27:20 +0200

    .

--- wwwsys/app/js/S2.Frameword/Core/JQuery.Module.js
+++
@@ -1,7 +0,0 @@
-import $ from 'https://code.jquery.com/jquery-3.6.0.min.js';
-
-// Esporta jQuery per renderlo disponibile ad altri moduli
-export { $ };
-
-// Logica aggiuntiva di Core2.js
-console.log("Core2.js caricato");
--- wwwsys/app/js/S2.Frameword/Core/Routing.js
+++
@@ -1,9 +0,0 @@
-// Sample routing code
-const routes = [
-    { path: "/",      component: HomeComponent },
-    { path: "/webTv", component: WebTvComponent },
-];
-
-function navigateTo(path) {
-    // Code to navigate to the specified route
-}
