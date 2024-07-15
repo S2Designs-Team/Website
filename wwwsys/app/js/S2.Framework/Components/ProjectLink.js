2ca2d2103f1ad25d920644c74c88fab81abf95b9
㊙️anonimo㊙️ <89788249+Phobetor1999@users.noreply.github.com>
2024-07-15 16:41:21 +0200

    .

--- wwwsys/app/js/S2.Framework/Components/ProjectLink.js
+++ wwwsys/app/js/S2.Framework/Components/ProjectLink.js
@@ -14,17 +14,17 @@
 # limitations under the License.
 #
 */
-export class ChannelLink extends BaseComponent {
-    channels = [];
+export class ProjectLink extends BaseComponent {
+    projects = [];
     chRoot = "wwwsys/pages/webTv/channels/";
 
     constructor(properties = {}, cssFileUrl = null) {
         super(properties, cssFileUrl); // Calls the Base Class constructor
 
-        if (ChannelLink.instance) {
-            return ChannelLink.instance;
+        if (ProjectLink.instance) {
+            return ProjectLink.instance;
         } 
-	    ChannelLink.instance = this;
+	    ProjectLink.instance = this;
     }
 
     initialize() {
@@ -34,94 +34,19 @@ export class ChannelLink extends BaseComponent {
     }
 
     initializeComponentData() {
-        this.channels[0]  = { on: false, type: "webTv", url: "TEST_dashJs_stream_mp4.html",       group: "S2",          iconStyleClass: "test-icon",       channelTitle: "S2 Test TV" }; 
-        this.channels[1]  = { on: false, type: "tv",    url: "rai_1.html",                        group: "RAI",         iconStyleClass: "rai1",            channelTitle: "Rai Uno" }; 
-        this.channels[2]  = { on: false, type: "tv",    url: "rai_2.html",                        group: "RAI",         iconStyleClass: "rai2",            channelTitle: "Rai Due" }; 
-        this.channels[3]  = { on: false, type: "tv",    url: "rai_3.html",                        group: "RAI",         iconStyleClass: "rai3",            channelTitle: "Rai Tre" }; 
-        this.channels[4]  = { on: true,  type: "tv",    url: "mediaset_rete_4.html",              group: "MEDIASET",    iconStyleClass: "rete4",           channelTitle: "Rete 4" };
-        this.channels[5]  = { on: true,  type: "tv",    url: "mediaset_canale_5.html",            group: "MEDIASET",    iconStyleClass: "canale5",         channelTitle: "Canale 5" };
-        this.channels[6]  = { on: true,  type: "tv",    url: "mediaset_italia_1.html",            group: "MEDIASET",    iconStyleClass: "italia1",         channelTitle: "Italia 1" };
-        this.channels[7]  = { on: true,  type: "tv",    url: "la7.html",                          group: "CAIRO",       iconStyleClass: "la7",             channelTitle: "La 7" };
-        this.channels[8]  = { on: true,  type: "tv",    url: "tv8.html",                          group: "N.A.",        iconStyleClass: "tv8",             channelTitle: "Tv 8" };
-        this.channels[9]  = { on: true,  type: "tv",    url: "discovery_+_nove.html",             group: "DISCOVERY+",  iconStyleClass: "nove",            channelTitle: "Nove" };
-        this.channels[10] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[11] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[12] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[13] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[14] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[15] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[16] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[17] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[18] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[19] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[20] = { on: true,  type: "tv",    url: "mediaset_20.html",                  group: "MEDIASET",    iconStyleClass: "_20",             channelTitle: "20" };
-        this.channels[21] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "rai4",            channelTitle: "Rai Quattro" }; 
-        this.channels[22] = { on: true,  type: "tv",    url: "mediaset_iris.html",                group: "MEDIASET",    iconStyleClass: "iris",            channelTitle: "Iris" };
-        this.channels[23] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "rai5",            channelTitle: "Rai Cinque" }; 
-        this.channels[24] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiMovie",        channelTitle: "Rai Movie" }; 
-        this.channels[25] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiPremium",      channelTitle: "Rai Premium" }; 
-        this.channels[26] = { on: false, type: "tv",    url: "",                                  group: "SKY",         iconStyleClass: "cielo",           channelTitle: "Cielo" }; 
-        this.channels[27] = { on: true,  type: "tv",    url: "mediaset_27.html",                  group: "MEDIASET",    iconStyleClass: "_27",             channelTitle: "27" };
-        this.channels[28] = { on: false, type: "tv",    url: "",                                  group: "N.A.",        iconStyleClass: "TV2000",          channelTitle: "Tv2000" }; 
-        this.channels[29] = { on: false, type: "tv",    url: "",                                  group: "CAIRO",       iconStyleClass: "la7d",            channelTitle: "La 7d" }; 
-        this.channels[30] = { on: true,  type: "tv",    url: "mediaset_la_5.html",                group: "MEDIASET",    iconStyleClass: "la5",             channelTitle: "La 5" };
-        this.channels[31] = { on: true,  type: "tv",    url: "discovery_realtime.html",           group: "DISCOVERY+",  iconStyleClass: "realTime",        channelTitle: "RealTime" };
-        this.channels[32] = { on: false, type: "tv",    url: "",                                  group: "N.A.",        iconStyleClass: "qvc",             channelTitle: "QVC" }; 
-        this.channels[33] = { on: true,  type: "tv",    url: "discovery_+_foodnetwork.html",      group: "DISCOVERY+",  iconStyleClass: "foodNetwork",     channelTitle: "Food Network" };
-        this.channels[34] = { on: true,  type: "tv",    url: "mediaset_cine_34.html",             group: "MEDIASET",    iconStyleClass: "cine34",          channelTitle: "Cine 34" };
-        this.channels[35] = { on: true,  type: "tv",    url: "mediaset_focus.html",               group: "MEDIASET",    iconStyleClass: "focus",           channelTitle: "Focus" };
-        this.channels[36] = { on: false, type: "radio", url: "",                                  group: "MEDIASET",    iconStyleClass: "rtl102.5",        channelTitle: "RTL 102.5 TV" }; 
-        this.channels[37] = { on: true,  type: "tv",    url: "mediaset_37.html",                  group: "MEDIASET",    iconStyleClass: "_37",             channelTitle: "37" }; 
-        this.channels[38] = { on: true,  type: "tv",    url: "discovery_+_giallo.html",           group: "DISCOVERY+",  iconStyleClass: "giallo",          channelTitle: "Giallo" };        
-        this.channels[39] = { on: true,  type: "tv",    url: "mediaset_top_crime.html",           group: "MEDIASET",    iconStyleClass: "topCrime",        channelTitle: "Top Crime" };
-        this.channels[40] = { on: true,  type: "tv",    url: "mediaset_boing.html",               group: "MEDIASET",    iconStyleClass: "boing",           channelTitle: "Boing" };
-        this.channels[41] = { on: true,  type: "tv",    url: "discovery_+_k2.html",               group: "DISCOVERY+",  iconStyleClass: "k2",              channelTitle: "K2" };
-        this.channels[42] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiGulp",         channelTitle: "Rai Gulp" }; 
-        this.channels[43] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiYoyo",         channelTitle: "Rai YoYo" }; 
-        this.channels[44] = { on: true,  type: "tv",    url: "discovery_+_frisbee.html",          group: "DISCOVERY+",  iconStyleClass: "frisbee",         channelTitle: "Frisbee" };
-        this.channels[45] = { on: false, type: "tv",    url: "",                                  group: "",            iconStyleClass: "",                channelTitle: "" };
-        this.channels[46] = { on: true,  type: "tv",    url: "mediaset_cartoonito.html",          group: "MEDIASET",    iconStyleClass: "cartoonito",      channelTitle: "Cartonito" };
-        this.channels[47] = { on: false, type: "tv",    url: "",                                  group: "VIACOM",      iconStyleClass: "super",           channelTitle: "Super!" }; 
-        this.channels[48] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiNews24",       channelTitle: "Rai News 24" }; 
-        this.channels[49] = { on: true,  type: "tv",    url: "mediaset_italia_2.html",            group: "MEDIASET",    iconStyleClass: "italia2",         channelTitle: "Italia 2" };
-        this.channels[50] = { on: false, type: "tv",    url: "",                                  group: "SKY",         iconStyleClass: "tg24",            channelTitle: "Sky TG24" }; 
-        this.channels[51] = { on: true,  type: "tv",    url: "mediaset_tgcom_24.html",            group: "MEDIASET",    iconStyleClass: "tgCom24",         channelTitle: "TGCom 24" };
-        this.channels[52] = { on: true,  type: "tv",    url: "discovery_+_dmax.html",             group: "DISCOVERY+",  iconStyleClass: "dmax",            channelTitle: "DMAX" };
-        this.channels[53] = { on: false, type: "tv",    url: "",                                  group: "CASA ITALIA", iconStyleClass: "casaItalia53",    channelTitle: "Canale Italia 53" }; 
-        this.channels[54] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiStoria",       channelTitle: "Rai Storia" }; 
-        this.channels[55] = { on: true,  type: "tv",    url: "mediaset_extra.html",               group: "MEDIASET",    iconStyleClass: "extra",           channelTitle: "Mediaset Extra" };
-        this.channels[56] = { on: true,  type: "tv",    url: "discovery_+_hgtv.html",             group: "DISCOVERY+",  iconStyleClass: "hgtv",            channelTitle: "HGTV" };
-        this.channels[57] = { on: false, type: "tv",    url: "",                                  group: "RAI",         iconStyleClass: "raiScuola",       channelTitle: "Rai Scuola" }; 
-        this.channels[59] = { on: true,  type: "tv",    url: "",                                  group: "DISCOVERY+",  iconStyleClass: "motorTrend",      channelTitle: "Motor Trend" }; 
-        this.channels[60] = { on: false, type: "tv",    url: "",                                  group: "PARAMOUNT",   iconStyleClass: "vh1",             channelTitle: "VH1" }; 
-        this.channels[61] = { on: true,  type: "radio", url: "mediaset_radio_101_tv.html",        group: "MEDIASET",    iconStyleClass: "radio101",        channelTitle: "Radio 101 TV" };
-        this.channels[62] = { on: true,  type: "radio", url: "mediaset_radio_105_tv.html",        group: "MEDIASET",    iconStyleClass: "radio105",        channelTitle: "Radio 105 TV" };
-        this.channels[63] = { on: true,  type: "radio", url: "mediaset_radio_virgin_tv.html",     group: "MEDIASET",    iconStyleClass: "virginRadio",     channelTitle: "VirginRadio TV" };
-        this.channels[64] = { on: true,  type: "radio", url: "mediaset_radio_montecarlo_tv.html", group: "MEDIASET",    iconStyleClass: "radioMonteCarlo", channelTitle: "Radio Montecarlo TV" };	
-        this.channels[65] = { on: true,  type: "webTv", url: "samsung_fail_army.html",            group: "",            iconStyleClass: "failArmy",        channelTitle: "Fail Army" };	
-        this.channels[66] = { on: true,  type: "webTv", url: "samsung_fuel_tv.html",              group: "",            iconStyleClass: "sofyTv",          channelTitle: "Fuel Tv" }; 
-        this.channels[67] = { on: false, type: "webTv", url: "samsung_teletubbies.html",          group: "RAKUTEN",     iconStyleClass: "teleTubbies",     channelTitle: "Teletubbies" }; 
-        this.channels[68] = { on: true,  type: "webTv", url: "rakuten_tv_azione.html",            group: "RAKUTEN",     iconStyleClass: "azione",          channelTitle: "Azione" };
-        this.channels[69] = { on: true,  type: "webTv", url: "rakuten_tv_cinema_italiano.html",   group: "RAKUTEN",     iconStyleClass: "cinemaItaliano",  channelTitle: "Cinema Italiano" };
-        this.channels[70] = { on: true,  type: "webTv", url: "rakuten_tv_commedia.html",          group: "RAKUTEN",     iconStyleClass: "commedia",        channelTitle: "Commedia" };
-        this.channels[71] = { on: true,  type: "webTv", url: "rakuten_tv_dramma.html",            group: "RAKUTEN",     iconStyleClass: "dramma",          channelTitle: "Dramma" };
-        this.channels[72] = { on: true,  type: "webTv", url: "rakuten_tv_film_top.html",          group: "RAKUTEN",     iconStyleClass: "filmTop",         channelTitle: "Film Top" };
-        this.channels[73] = { on: true,  type: "webTv", url: "rakuten_tv_romance.html",           group: "RAKUTEN",     iconStyleClass: "romance",         channelTitle: "Romance" };
-        this.channels[74] = { on: true,  type: "webTv", url: "rakuten_tv_family.html",            group: "RAKUTEN",     iconStyleClass: "family",          channelTitle: "Family" };
-        this.channels[75] = { on: true,  type: "webTv", url: "rakuten_tv_cineWestern.html",       group: "RAKUTEN",     iconStyleClass: "cineWestern",     channelTitle: "Cine Western" };
-        this.channels[76] = { on: true,  type: "webTv", url: "rakuten_tv_andromeda.html",         group: "RAKUTEN",     iconStyleClass: "andromeda",       channelTitle: "Andromeda" };
-        this.channels[77] = { on: true,  type: "webTv", url: "rakuten_tv_sciFi.html",             group: "RAKUTEN",     iconStyleClass: "sciFi",           channelTitle: "Sci-Fi" };
-        this.channels[78] = { on: true,  type: "webTv", url: "rakuten_tv_fantascienza.html",      group: "RAKUTEN",     iconStyleClass: "drWho",           channelTitle: "Dr. WHO" };
+        this.projects[0]  = { on: true,  type: "3D",    url: "TEST_dashJs_stream_mp4.html",       group: "3D",         iconStyleClass: "test-icon",       projectTitle: "Ready Player Me" }; 
+        this.projects[1]  = { on: true,  type: "3D",    url: "rai_1.html",                        group: "3D",         iconStyleClass: "rai1",            projectTitle: "I AM MAI" }; 
+        this.projects[2]  = { on: true,  type: "3D",    url: "rai_2.html",                        group: "3D",         iconStyleClass: "rai2",            projectTitle: "Unreal Engine 5.4.2 Guide" }; 
     }
 
     initializeGui() {
-        this.htmlSegment = `<UL CLASS="channelList">`;
-        this.channels.forEach((element) => {
+        this.htmlSegment = `<UL CLASS="projectList">`;
+        this.projects.forEach((element) => {
             if (element.on === true) {
                 this.htmlSegment += 
                 `
-                    <LI ID="${element.channelTitle}">
-                        <A CLASS="channelLink" HREF='${this.chRoot}${element.url}'>
+                    <LI ID="${element.projectTitle}">
+                        <A CLASS="projectLink" HREF='${this.chRoot}${element.url}'>
                             <DIV CLASS="icon ${element.iconStyleClass}" ID="${element.iconStyleClass}"></DIV>
                             <DIV CLASS="truncate"  ID="title">${element.channelTitle}</DIV>
                         </A>
@@ -137,10 +62,10 @@ export class ChannelLink extends BaseComponent {
     addEventListeners(container) {
         // Adds an 'On clickEvent' to all hyperlinks inside '.channelList' element
         // then anonymous function will be called
-        $(".channelLink").click(function () {
+        $(".projectLink").click(function () {
             console.clear();
             var url     = $(this).attr('href');
-            $('.channelList li').removeClass('selected');
+            $('.projectList li').removeClass('selected');
             $(this).parent().addClass('selected');
 	
             /*👉️ [SNI] ONLY FOR DEBUG MODE UNCOMMENT THE FOLLOWING CODE:
@@ -167,8 +92,8 @@ export class ChannelLink extends BaseComponent {
                     alert("Error: " + jqXHR.status + " " + jqXHR.statusText);
                 }
             });
-            selectedChannelName = $(this).parent().attr('id');
-            console.log("Selected channel: '" + selectedChannelName + "'");
+            selectedProjectName = $(this).parent().attr('id');
+            console.log("Selected project: '" + selectedProjectName + "'");
             return false;
         });
     }
